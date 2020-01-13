5eb2ce4b0a4035d4d423391de421e5cb843316f7
brandon moro <brandon_078@hotmail.com>
2020-01-13 22:22:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,7 @@
 
 def key_for_min_value(name_hash)
   lowest_key = nil
-  lowest_value = nil 
+  lowest_value = nil
 name_hash.each do |key, value|
   if lowest_value == nil || value < lowest_value
     lowest_value = v
@@ -12,4 +12,3 @@ name_hash.each do |key, value|
   end
 lowest_key
 end
-
